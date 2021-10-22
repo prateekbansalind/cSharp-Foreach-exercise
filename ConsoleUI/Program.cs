a0b15bd7de98d3477f1501cf1128a959f5ce8802
pbansal16 <prateekbansalind@gmail.com>
2021-10-22 15:30:51 +1300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> firstNames = new List<string>();
+            string decisionVar;
+            do
+            {
+                // Ask the user for their first name.
+                Console.Write("Please enter your name: ");
+                string firstName = Console.ReadLine();
+
+                // Continue asking for their first names until there are no more.
+                Console.Write("Do you want to make another entry (y/n): ");
+                decisionVar = Console.ReadLine();
+
+
+                firstNames.Add(firstName); 
+            } while (decisionVar.ToLower() == "y");
+
+            // Then loop through each name using foreach and tell them hello.
+            foreach (string firstName in firstNames)
+            {
+                Console.WriteLine($"Hello, {firstName}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
